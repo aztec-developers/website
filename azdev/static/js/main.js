c7a3acb239c26a7d8a6a5f3fcffb53bb9d04e8ab
Michael Green <me@michaelgreen.net>
2017-01-09 16:03:52 -0800

    progression with parallax

--- azdev/static/js/main.js
+++ azdev/static/js/main.js
@@ -1,18 +1,26 @@
-var mainScene = document.getElementById('main-scene');
+var mainScene = document.getElementById("main-scene");
 var parallax = new Parallax(mainScene, {
-  invertY: true,
-  invertX: true,
-  limitx:false,
-  limity:false
-
+    invertY: true,
+    invertX: true,
+    frictionX: .7,
+    frictionY: .5,
+    originY: .7,
+    calibrateX: true,
+    calibrateY: true
 });
 
 
-var subScene = document.getElementById('sub-scene');
+var subScene = document.getElementById("sub-scene");
 var parallax = new Parallax(subScene, {
-  invertY: true,
-  invertX: true,
-  limitx:false,
-  limity:false,
-  scaley:5
+    invertY: true,
+    invertX: true,
+    limitx: 4,
+    limity: false,
+    frictionX: .4,
+    frictionY: .4,
+    originY: .7,
+    originX:.7,
+    scaleY: 1,
+    calibrateX: true,
+    calibrateY: true,
 });
