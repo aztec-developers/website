5e07333a1c5744eda112a77cd7e2668cdce2b792
Michael Green <me@michaelgreen.net>
2017-02-01 03:33:18 -0800

    Way better looking nav and front page

--- azdev/static/js/main.js
+++ azdev/static/js/main.js
@@ -1,5 +1,16 @@
-var mainScene = document.getElementById("main-scene");
-var parallax = new Parallax(mainScene, {
+var titleScene = document.getElementById("main-scene");
+var titleParallax = new Parallax(titleScene, {
+    invertY: true,
+    invertX: true,
+    frictionX: 1.0,
+    frictionY: 1.0,
+    calibrateX: true,
+    calibrateY: true
+});
+
+
+var contentScene = document.getElementById("content-scene");
+var contentParallax = new Parallax(contentScene, {
     invertY: true,
     invertX: true,
     frictionX: 1.0,
