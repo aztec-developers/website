d4bb681ba3f2cacda1c9d7e16befb86bc58deb02
Michael Green <me@michaelgreen.net>
2017-02-02 14:43:57 -0800

    section animation

--- azdev/static/js/main.js
+++ azdev/static/js/main.js
@@ -1,20 +1,20 @@
-var titleScene = document.getElementById("main-scene");
-var titleParallax = new Parallax(titleScene, {
-    invertY: true,
-    invertX: true,
-    frictionX: 1.0,
-    frictionY: 1.0,
-    calibrateX: true,
-    calibrateY: true
-});
+// var titleScene = document.getElementById("main-scene");
+// var titleParallax = new Parallax(titleScene, {
+//     invertY: true,
+//     invertX: true,
+//     frictionX: 1.0,
+//     frictionY: 1.0,
+//     calibrateX: true,
+//     calibrateY: true
+// });
 
 
-var contentScene = document.getElementById("content-scene");
-var contentParallax = new Parallax(contentScene, {
-    invertY: true,
-    invertX: true,
-    frictionX: 1.0,
-    frictionY: 1.0,
-    calibrateX: true,
-    calibrateY: true
-});
+// var contentScene = document.getElementById("content-scene");
+// var contentParallax = new Parallax(contentScene, {
+//     invertY: true,
+//     invertX: true,
+//     frictionX: 1.0,
+//     frictionY: 1.0,
+//     calibrateX: true,
+//     calibrateY: true
+// });
