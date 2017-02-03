4f65e61beabf5320e0c2a9dbd6a36729c45db89c
Michael Green <me@michaelgreen.net>
2017-02-02 16:45:57 -0800

    Damn reserved keywords are the worst, finally got client side routing to work

--- azdev/app.js
+++ azdev/app.js
@@ -7,10 +7,14 @@ var router = express.Router();
 
 /* Static assets, CSS, Images, bower components, etc. */
 app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/public'));
 
+// return index.html for all other routes
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
+
+
 
 /* Need to do this fuck. SSR as a backup to client shit. */
 // app.get('/about', function(req, res) {
