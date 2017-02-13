5d6816fd1974231383273dd2d19276b2a721df87
Michael Green <me@michaelgreen.net>
2017-02-13 11:52:19 -0800

    Fixing unadded directory

---
+++ app/build/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// simple express server
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+/* Static assets, CSS, Images, bower components, etc. */
+app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/public'));
+
+// return index.html for all other routes
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(1338);
