abeebc3ea910f5fff0b1de16d9a23f83f23a4c93
Matthew Yamout <Matthew Yamout>
2017-02-15 22:05:51 -0800

    Something broke on this branch...

--- app/build/app.js
+++ app/build/app.js
@@ -4,6 +4,9 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Static assets, CSS, Images, bower components, etc. */
 app.use(express.static(__dirname + '/static'));
@@ -12,6 +15,16 @@ app.use(express.static(__dirname + '/public'));
 // return index.html for all other routes
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/contact_us', function(req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+  console.log(name);
+  console.log(email);
+  console.log(message);
+  res.send('message sent!');
 });
 
 app.listen(1338);
